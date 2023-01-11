2f6406f00c1ae15cf307956ef633155cf309fb6b
Huy Nguyen <bob@nucamp.co>
2023-01-11 14:39:11 -0600

    access origin for all

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const cors = require('cors');
 const port = 8080;
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "*",
+}));
 app.use(express.json());
 app.use(require('./apiRoute'));
 
